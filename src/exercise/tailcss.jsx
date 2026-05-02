94e74e6f74cf3e31f8049aea90dd3e00ecf56501
Nuelwhiz <emmanuelifeanyi668@gmail.com>
2026-05-02 08:40:58 +0100

    fixed issues of spacing on shrink

--- src/exercise/tailcss.jsx
+++ src/exercise/tailcss.jsx
@@ -8,48 +8,51 @@ export default function Tail() {
   return (
     <>
       <main className="">
-        <div className="flex  bg-slate-800 lg:h-screen w-full justify-between">
+        <div className="flex  bg-slate-800 lg:h-screen w-full  ">
           <aside
-            className={` hidden md:flex flex-col gap-5  h-full bg-slate-700 border-r  border-slate-300  ${open ? "w-[10%]" : "w-15%]"}`}
+            className={` hidden md:flex flex-col gap-5  h-full bg-slate-700 border-r 
+               border-slate-300  transition-all duration-300  ${open ? "w-[20%]" : "w-[10%]"}`}
           >
             <button
               className="h-8 w-8 bg-rose-400  rounded ml-6 mt-2"
               onClick={tuggle}
             ></button>
             <div
-              className={`h-8  bg-slate-500  rounded self-center flex px-2 items-center mt-4 ${open ? "w-20" : "w-45"}`}
+              className={`h-8  bg-slate-500  rounded self-center flex px-2 items-center mt-4 ${open ? "w-45" : "w-15"}`}
             >
-              <div className="h-4 w-20 bg-slate-400  rounded  "></div>
+              <div
+                className={`h-4 w-20 bg-slate-400  rounded ${open ? "w-20" : ""}  `}
+              ></div>
             </div>
             <div
-              className={`h-4 bg-blue-400  rounded ml-5 ${open ? "w-8" : "w-20"}`}
+              className={`h-4 bg-blue-400  rounded ml-5 ${open ? "w-20" : "w-4"}`}
             ></div>
             <div
-              className={`h-4 ${open ? "w-8" : "w-20 ml-5 "} bg-rose-400  rounded  `}
+              className={`h-4 ${open ? "w-20" : "w-4 "} bg-rose-400  rounded ml-5   `}
             ></div>
             <div
-              className={`h-4 ${open ? "w-8" : "w-20"} bg-green-400  rounded ml-8  `}
+              className={`h-4 ${open ? "w-20" : "w-4"} bg-green-400  rounded ml-8  `}
             ></div>
             <div
-              className={`h-4 ${open ? "w-8" : "w-20"} bg-orange-400  rounded ml-4 `}
+              className={`h-4 ${open ? "w-20" : "w-4"} bg-orange-400  rounded ml-4 `}
             ></div>
             <div
-              className={`h-4 ${open ? "w-8" : "w-20"} bg-purple-400  rounded ml-4  `}
+              className={`h-4 ${open ? "w-20" : "w-4"} bg-purple-400  rounded ml-4  `}
             ></div>
             <div
-              className={`h-4 ${open ? "w-8" : "w-20"} bg-purple-600  rounded ml-4  `}
+              className={`h-4 ${open ? "w-20" : "w-4"} bg-purple-600  rounded ml-4  `}
             ></div>
             <div
-              className={`h-4 ${open ? "w-8" : "w-20"} bg-slate-500  rounded ml-4  `}
+              className={`h-4 ${open ? "w-20" : "w-4"} bg-slate-500  rounded ml-4  `}
             ></div>
             <div
-              className={`h-4 ${open ? "w-8" : "w-20"} bg-slate-500  rounded ml-4  `}
+              className={`h-4 ${open ? "w-20" : "w-4"} bg-slate-500  rounded ml-4  `}
             ></div>
           </aside>
-          <section className="flex flex-1 flex-col gap-4 w-80%  ">
-            <header className="h-14 w-full border-b border-slate-300 bg-slate-700  flex items-center flex-wrap justify-between ">
-              <div className="h-8 w-40 md:w-70 bg-slate-400 ml-5 rounded "></div>
-              <div className="flex items-center gap-3 mr-5">
+          <section className="flex flex-1 overflow-hidden  flex-col  gap-5   ">
+            <header className="h-14 w-full border-b border-slate-300 bg-slate-700  flex items-center flex-wrap justify-between  px-5 ">
+              <div className="h-8 w-40 md:w-70 bg-slate-400  rounded "></div>
+              <div className="flex items-center gap-3 ">
                 <div className="h-8 w-6 md:w-8 bg-slate-400  rounded  "></div>
                 <div className="h-8 w-6 mm:w-8 bg-slate-400  rounded  "></div>
                 <div className="h-8 w-8 md:w-12 bg-slate-400  rounded  "></div>
@@ -60,8 +63,8 @@ export default function Tail() {
               </div>
             </header>
             {/* card section */}
-            <section className="flex justify-between px-5  mt-7 w-full flex-wrap justify-around ">
-              <div className=" flex h-20 w-54 md:w-67.5 bg-blue-500   rounded justify-between p-3 ">
+            <section className="flex   mt-7 w-full flex-wrap md:flex-nowrap px-5 gap-5   ">
+              <div className=" flex h-20 w-full md:w-[22%]  bg-blue-500 flex-1   rounded justify-between p-3 ">
                 <div className="flex flex-col gap-2 justify-center">
                   <div className="w-25 h-3 bg-slate-600 rounded"></div>
                   <div className="w-15 h-3 bg-slate-200 rounded"></div>
@@ -75,7 +78,7 @@ export default function Tail() {
                 </div>
               </div>
 
-              <div className="flex h-20 w-54 md:w-67.5 bg-green-500  rounded justify-between p-3">
+              <div className="flex h-20 w-full md:w-[22%]  bg-green-500 flex-1  rounded justify-between p-3">
                 <div className="flex flex-col gap-2 justify-center">
                   <div className="w-25 h-3 bg-slate-600 rounded"></div>
                   <div className="w-15 h-3 bg-slate-200 rounded"></div>
@@ -89,7 +92,7 @@ export default function Tail() {
                 </div>
               </div>
 
-              <div className="flex h-20 w-54 md:w-67.5 bg-orange-500  rounded justify-between p-3">
+              <div className="flex h-20 w-full md:w-[22%]  bg-orange-500  flex-1 rounded justify-between p-3">
                 <div className="flex flex-col gap-2 justify-center">
                   <div className="w-25 h-3 bg-slate-600 rounded"></div>
                   <div className="w-15 h-3 bg-slate-200 rounded"></div>
@@ -103,7 +106,7 @@ export default function Tail() {
                 </div>
               </div>
 
-              <div className="flex justify-between p-3 h-20 w-54 md:w-67.5 bg-purple-500  rounded ">
+              <div className="flex justify-between p-3 h-20 w-full md:w-[22%]  bg-purple-500 flex-1 rounded ">
                 <div className="flex flex-col gap-2 justify-center">
                   <div className="w-25 h-3 bg-slate-600 rounded"></div>
                   <div className="w-15 h-3 bg-slate-200 rounded"></div>
@@ -118,35 +121,10 @@ export default function Tail() {
               </div>
             </section>
 
-            <section className="flex justify-between px-5 flex-wrap jusify-center items-center">
-              <div className="h-54 w-full md:w-139 bg-slate-700  rounded p-2 grid grid-cols-6 justify-center items-center ">
-                <div className="w-17 h-10 border border-gray-600 "></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-                <div className="w-17 h-10 border border-gray-600"></div>
-              </div>
+            <section className="flex  flex-wrap md:flex-nowrap jusify-center items-center px-5 gap-4">
+              <div className="h-54 w-full md:w-[45%] bg-slate-700  rounded p-2 flex flex-1 justify-center items-center  "></div>
 
-              <div className="h-54  w-full md:w-139 bg-slate-700  rounded sm:my-5 md:my-0 ">
+              <div className="h-54  w-full md:w-[45%] bg-slate-700  rounded  ">
                 <div className=" flex  justify-between w-full  bg-slate-300 p-2 mt-12">
                   <div className="w-30 h-3 bg-slate-600 rounded"></div>
                   <div className="w-30 h-3 bg-slate-600 rounded"></div>
@@ -174,8 +152,8 @@ export default function Tail() {
               </div>
             </section>
 
-            <section className="flex  flex-wrap w-full px-5  md:px-5  md:justify-between ">
-              <div className="h-44 w-full  md:w-91.5 bg-slate-700   rounded p-3 ">
+            <section className="flex  flex-wrap md:flex-nowrap w-full px-5 gap-4  ">
+              <div className="h-44 w-full  md:w-[30%] flex-1 bg-slate-700   rounded p-3 ">
                 <div className=" flex  justify-between w-full  rounded">
                   <div className="w-25 h-3 bg-slate-300 rounded"></div>
                 </div>
@@ -201,7 +179,7 @@ export default function Tail() {
                 </div>
               </div>
 
-              <div className="h-44 w-full md:w-91.5 bg-slate-700  rounded p-3  ">
+              <div className="h-44 w-full md:w-[30%] flex-1 bg-slate-700  rounded p-3  ">
                 <div className=" flex  justify-between w-full  rounded">
                   <div className="w-25 h-3 bg-slate-300 rounded"></div>
                 </div>
@@ -229,7 +207,7 @@ export default function Tail() {
                 </div>
               </div>
 
-              <div className="h-44 w-full md:w-91.5 bg-slate-700  rounded p-3 ">
+              <div className="h-44 w-full md:w-[30%] flex-1 bg-slate-700  rounded p-3 ">
                 <div className=" flex  justify-between w-full  rounded">
                   <div className="w-25 h-3 bg-slate-300 rounded"></div>
                 </div>
